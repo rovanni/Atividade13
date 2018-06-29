08f56b65481085beec8482acf0ab895bf162039d
Luciano <rovanni@gmail.com>
2018-06-29 11:20:39 -0300

    Refatorando Código

--- src/test/java/br/edu/utfpr/login/FindLoginPage.java
+++ src/test/java/br/edu/utfpr/login/FindLoginPage.java
@@ -1,8 +1,3 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package br.edu.utfpr.login;
 
 import org.openqa.selenium.WebDriver;
@@ -12,25 +7,6 @@ import org.openqa.selenium.WebDriver;
  * @author Luciano Rovanni <luciano at luciano.rovanni>
  */
 public class FindLoginPage extends BasePage {
-//
-//    @FindBy(xpath = "/html/body/div/div/div[1]/div[3]/div[2]/h2")
-//    WebElement title;
-//
-//    @FindBy(xpath = "//*[@id=\"errorExplanation\"]/ul/li[1]")
-//    WebElement erroEmail;
-//
-//    @FindBy(xpath = "//*[@id=\"errorExplanation\"]/ul/li[2]")
-//    WebElement erroLogin;
-//
-//    @FindBy(xpath = "//*[@id=\"errorExplanation\"]/ul/li[3]")
-//    WebElement erroFirstName;
-//
-//    @FindBy(xpath = "//*[@id=\"errorExplanation\"]/ul/li[4]")
-//    WebElement erroLastName;
-//
-//    @FindBy(xpath = "//*[@id=\"errorExplanation\"]/ul/li[5]")
-//    WebElement erroPasssword;
-//
 
     WebDriver dr;
 
@@ -39,34 +15,6 @@ public class FindLoginPage extends BasePage {
         dr = driver;
     }
 
-//    public AddLoginPage goToAddCadastro() {
-//        //btnEnviar.click();
-//
-//        return new AddLoginPage(driver);
-//    }
-//
-//    public WebElement getTitle() {
-//        return title;
-//    }
-//
-//    public boolean validCadastro() {
-//        if (erroEmail != null) {
-//            return false;
-//        }
-//        if (erroFirstName != null) {
-//            return false;
-//        }
-//        if (erroLastName != null) {
-//            return false;
-//        }
-//        if (erroLogin != null) {
-//            return false;
-//        }
-//        if (erroPasssword != null) {
-//            return false;
-//        }
-//        return true;
-//    }
     public boolean loginSucesso() {
         switch (dr.getCurrentUrl()) {
             case "http://demo.redmine.org/my/page":
